f4fccb1e79eb67a71285bd87a16d7125f666b437
Jurriën Dokter <jurrien@ximsystems.nl>
2019-01-23 07:48:47 +0100

    Being a bit creative with importing and booru-checking

--- src/lib/components/partials/SettingsDialog/index.js
+++ src/lib/components/partials/SettingsDialog/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import _ from 'lodash'
 
 import Dialog from '../Dialog'
-import { renameKeyword } from '../../../store/modules/search/api'
+import { renameKeyword, getSummary, update } from '../../../store/modules/search/api'
 
 import './style.scss'
+
+const promiseSerial = funcs =>
+    funcs.reduce((promise, func) =>
+        promise.then(result => func().then(Array.prototype.concat.bind(result))),
+    Promise.resolve([]))
+
+const CHECKED_ON_BOORU = 'checked_on_e621'
 
 export default class SettingsDialog extends React.Component {
     static propTypes = {
@@ -18,11 +26,37 @@ export default class SettingsDialog extends React.Component {
     state = {
         pending: false,
         success: false,
-        error: null
+        error: null,
+        count: 0,
+        total: 0
     }
 
     render() {
-        return <Dialog title={'Utilities'} onRequestClose={this.props.onRequestClose} />
+        const percentage = this.state.total === 0
+            ? 0
+            : Math.floor((this.state.count / this.state.total) * 100)
+
+        return (
+            <Dialog title={'Utilities'} killPropagation onRequestClose={this.props.onRequestClose}>
+                <div className={'grid-x'}>
+                    <div className={'cell auto'}>
+                        <div className={'progress success'} role={'progressbar'}>
+                            <div className='progress-meter' style={{ width: `${percentage}%` }}>
+                                <p className={'progress-meter-text'}>{this.state.count} / {this.state.total}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button className={'button'} onClick={event => {
+                                event.stopPropagation()
+                                event.preventDefault()
+
+                                this._queueDocs()
+                            }}>Check with E621</button>
+                        </div>
+                    </div>
+                </div>
+            </Dialog>
+        )
     }
 
     _renameTag = (oldTag, newTag) => {
@@ -40,4 +74,98 @@ export default class SettingsDialog extends React.Component {
                 })
             })
     }
+
+    // queue docs for checking with booru
+    _queueDocs = () => {
+        getSummary(['checksum', 'keywords']).then(sums => {
+            console.info('-- checksums:result', sums, sums.length)
+
+            this.setState({
+                total: sums.length
+            })
+
+            const funcs = sums.map((data, index) => {
+                return () => {
+                    return this._checkBooru({
+                        keywords: [],
+                        ...data
+                    })
+                }
+            })
+
+            promiseSerial(funcs)
+                .then(finalResult => {
+                    console.info('-- finally done!', finalResult)
+                })
+                .catch(err => {
+                    console.info('-- it failed', err)
+                })
+        })
+    }
+
+    // check booru, I know.. it's cheap
+    _checkBooru = (item) => new Promise(resolve => {
+        if (item.keywords.indexOf(CHECKED_ON_BOORU) > -1) {
+            this.setState(state => {
+                console.info('-- state:n', state.total, state.count, state.count / state.total)
+                return {
+                    count: state.count + 1
+                }
+            }, () => {
+                resolve(item)
+            })
+            return
+        }
+
+        setTimeout(() => {
+            fetch(`booru://e621/${item.checksum}`)
+                .then(result => result.json())
+                .then(result => {
+                    if (!result.id) {
+                        const keywords = [...item.keywords, CHECKED_ON_BOORU]
+                        item.keywords = _.uniq(keywords)
+
+                        resolve(this._update(item.id, item))
+                    } else {
+                        item.author = result.artist
+                        item.source = result.source
+                        item.rating = (rating => {
+                            switch (rating) {
+                                case 'q': return 'questionable'
+                                case 's': return 'safe'
+                                case 'e': return 'explicit'
+                            }
+                        })(result.rating)
+
+                        const keywords = [...item.keywords, ...result.tags.split(/\s+/g), 'e621', CHECKED_ON_BOORU]
+
+                        item.keywords = _.uniq(keywords)
+
+                        resolve(this._update(item.id, item))
+                    }
+                })
+        }, 2500)
+    })
+
+    _update = (id, data) => new Promise(resolve => {
+        const query = {
+            id,
+            index: 'media',
+            type: 'media',
+            body: {
+                doc: data
+            }
+        }
+
+        update(query).then(() => {
+            this.setState(state => {
+                console.info('-- state:u', state.total, state.count, state.count / state.total)
+                return {
+                    count: state.count + 1
+                }
+            }, () => {
+                resolve(id)
+            })
+        })
+    })
 }
---
+++ src/lib/store/includes/client.js
@@ -0,0 +1,76 @@
+import elastic from 'elasticsearch'
+import _ from 'lodash'
+
+export default class Client {
+    constructor(config) {
+        this._index = config.index
+        this._mapping = [
+            { // always used
+                favorite: {
+                    type: 'boolean'
+                }
+            }
+        ]
+        this._client = new elastic.Client(config.search)
+    }
+
+    ping(opt) {
+        return this._client.ping(opt)
+    }
+
+    get indices() {
+        return this._client.indices
+    }
+
+    index(opt) {
+        return this._client.index(opt)
+    }
+
+    update(opt) {
+        return this._client.update(opt)
+    }
+
+    delete(id) {
+        return this._client.delete({
+            index: this._index,
+            type: 'media',
+            id
+        })
+    }
+
+    search(opt) {
+        return this._client.search(opt)
+    }
+
+    scroll(opt) {
+        let documents = []
+        const client = this._client
+
+        return new Promise((resolve, reject) => {
+            client.search({
+                index: this._index,
+                type: 'media',
+                scroll: '30s',
+                size: 250,
+                ...opt
+            }, function untildone(error, response) {
+                if (error) {
+                    reject(error)
+                }
+
+                // console.info('-- response', response)
+
+                documents = _.concat(documents, response.hits.hits)
+
+                if (response.hits.total > documents.length) {
+                    client.scroll({
+                        scrollId: response._scroll_id,
+                        scroll: '30s'
+                    }, untildone)
+                } else {
+                    resolve(documents)
+                }
+            })
+        })
+    }
+}
